198a4cf2aadad991050c90c3575eba62d9b54152
Thiago Rossi <>
2022-08-04 01:22:37 -0300

    add relationship rules

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Device < ApplicationRecord
+  has_many :inventories, dependent: :destroy
+  has_many :users, through: :inventories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   belongs_to :country
+  has_many :inventories, dependent: :destroy
+  has_many :devices, through: :inventories
+
+  scope :available, ->(vacation) { where(vacation: !vacation) }
+
+  def total_devices
+    inventories.sum(&:quantity)
+  end
 end
